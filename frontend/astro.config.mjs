836543fb0d69dbec6e89e0b9491e1ff309752329
Minju Jeon <odeto1310@gmail.com>
2025-01-09 10:05:07 +0900

    Fix : Astro - vue integration
    1. Astro-Vue 통합
    2. url 루트 변경 - navbar 경로 수정

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
-  base: '/astro-ecommerce'
+    integrations: [react(), vue()],
+    site: 'https://creativetimofficial.github.io',
+    base: '/ninescent',
 });
